c29610d10bdca356117572541c7440ef0594a1fb
Christian Berndt <christian.berndt@liferay.com>
2020-06-05 13:38:31 +0200

    Remove obsolete comment.

--- liferay-upgrade-process-service/src/main/java/com/chberndt/liferay/upgrade/process/service/internal/upgrade/v1_1_0/UpgradeFoo.java
+++ liferay-upgrade-process-service/src/main/java/com/chberndt/liferay/upgrade/process/service/internal/upgrade/v1_1_0/UpgradeFoo.java
@@ -1,7 +1,5 @@
 package com.chberndt.liferay.upgrade.process.service.internal.upgrade.v1_1_0;
 
-import com.chberndt.liferay.upgrade.process.model.Foo;
-
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 
 /**
@@ -11,10 +9,8 @@ public class UpgradeFoo extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		
-		// alter(Foo.class, new AlterTableAddColumn("FOO_FOO field6 VARCHAR(255)"));
-		
-		runSQL(connection, "ALTER TABLE FOO_FOO ADD COLUMN field6 VARCHAR(255)");
+		runSQL(
+			connection, "ALTER TABLE FOO_FOO ADD COLUMN field6 VARCHAR(255)");
 	}
 
 }
